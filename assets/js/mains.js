a9ba6bbb48792cd2f29d9bb174cfa08d8d753dcb
Cass <douglaszatara1234@gmail.com>
2023-08-26 19:15:54 -0300

    Atualizando numero,nome,atributos e imagens

--- assets/js/mains.js
+++ assets/js/mains.js
@@ -1,16 +1,23 @@
 
+
+function convertPokemonTypesToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlot)=> ` <li class="type">${typeSlot.type.name}</li>`)
+
+}
+
 function converPokemonToLI(pokemon){
     return `
     <li class="pokemon">
-    <span class="number">#001</span>
+    <span class="number">#${pokemon.order}</span>
     <span class="name">${pokemon.name}</span>
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${convertPokemonTypesToLi(pokemon.types).join(' ')}
         </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-    </div>
+       
+        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+    
+        </div>
 </li>
     
     
@@ -21,8 +28,8 @@ function converPokemonToLI(pokemon){
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(converPokemonToLI).join('')
-
+   const newHtml = pokemons.map(converPokemonToLI).join('')
+pokemonList.innerHTML = newHtml
  
     })
 
